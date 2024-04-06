4d7a5c48928b05e6c92b222fdda4faf979a60aff
Przemek Majewski <dzius89@gmail.com>
2024-04-06 14:29:06 +0200

    create logic to get subjects with their grades and then calculate average

---
+++ Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Program to calculate the arithmetic average of grades from various subjects.");
+
+        System.out.print("Enter the number of subjects: ");
+        int numberOfSubjects = scanner.nextInt();
+
+        double[][] grades = new double[numberOfSubjects][];
+
+        for(int i = 0; i < numberOfSubjects; i++) {
+            System.out.print("Enter the number of grades for subject " + (i + 1) + ": ");
+            int numberOfGrades = scanner.nextInt();
+            grades[i] = new double[numberOfGrades];
+
+            for(int j = 0; j < numberOfGrades; j++) {
+                System.out.print("Enter grade number " + (j + 1) + ": ");
+                grades[i][j] = scanner.nextDouble();
+            }
+        }
+
+        double sum = 0;
+        int totalNumberOfGrades = 0;
+        for (double[] grade : grades) {
+            for (double v : grade) {
+                sum += v;
+                totalNumberOfGrades++;
+            }
+        }
+        double average = sum / totalNumberOfGrades;
+
+        System.out.print("The average grade is " + average);
+    }
+}
